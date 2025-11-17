3399fa632ba6e8345315648aa1f135e6487e2367
Emree <92224549+Emre9900@users.noreply.github.com>
2025-11-17 15:13:54 +0300

    Add LoginPanel, DashBoard, and Masalar forms
    Introduced new forms: LoginPanel, DashBoard, and Masalar, including their designer and resource files. Updated the project file to include these forms and removed the old Form1 files. Adjusted document layout and related metadata to reflect the new structure.

---
+++ KafeOtomasyonu/LoginPanel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KafeOtomasyonu
+{
+    public partial class LoginPanel : Form
+    {
+
+        DashBoard DashBoard = new DashBoard();
+        public LoginPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtKullaniciAdi.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (BackColor == Color.White)
+            {
+                ForeColor = Color.White;
+                BackColor = Color.Black;
+                btnGiris.ForeColor = Color.White;
+                btnTheme.ForeColor = Color.White;
+            }
+            else
+            {
+                ForeColor = Color.Black;
+                BackColor = Color.White;
+                btnGiris.ForeColor = Color.Black;
+                btnTheme.ForeColor = Color.Black;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtKullaniciAdi.Text == "Emre" && txtSifre.Text == "1234")
+            {
+                MessageBox.Show("Giriş Başarılı, Dashboard paneline yönlendiriliyorsunuz. ", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DashBoard.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı veya Şifre Hatalı! ", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        }
+    }
+
--- KafeOtomasyonu/Masalar.cs
+++ KafeOtomasyonu/Masalar.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace KafeOtomasyonu
 {
-    public partial class Form1 : Form
+    public partial class Masalar : Form
     {
-        public Form1()
+        public Masalar()
         {
             InitializeComponent();
         }
